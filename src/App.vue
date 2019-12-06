6c5097a6317359cd26615cf9af1971195ff31e7d
Saivenkatesh Manoharan <saivenkateshm@CESCHN.COM>
2019-12-06 18:58:41 +0530

    initial commit with dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <Header></Header>
+    <v-content>
+
+      fsdfsdfsd d as 
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Header from "./components/Header"
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
+  name: "App",
+
+  components: {Header},
+
+  data: () => ({})
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app-bar app >
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+
+      <v-img
+        alt="Vuetify Name"
+        class="shrink mt-1 hidden-sm-and-down"
+        contain
+        min-width="100"
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+        width="100"
+      />
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-switch class="theme-select mt-5" @change="setTheme" v-model="goDark"></v-switch>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data: () => ({
+    goDark: false
+  }),
+  methods: {
+    setTheme() {
+      if (this.goDark == true) {
+        return (this.$vuetify.theme.dark = true);
+      } else {
+        return (this.$vuetify.theme.dark = false);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
